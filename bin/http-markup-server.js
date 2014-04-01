774fde118322e577f785503759d02f21cdd26ac3
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-04-01 18:18:40 +0400

    ignore spawn browser error

--- bin/http-markup-server.js
+++ bin/http-markup-server.js
@@ -214,7 +214,14 @@ app.listen(port, host, function () {
     }
 
     if (port !== 80) URL += ':' + port;
-    if (openInBrowser) spawn('xdg-open', [URL], { stdio: 'ignore' });
+    if (openInBrowser) {
+
+        spawn('xdg-opens', [URL], { stdio: 'ignore' })
+            .on('error', function (err) {
+                console.log('Open link in browser error:', err.toString());
+            });
+
+    }
 
 });
 
