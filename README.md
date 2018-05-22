55835475951e3d0d189b0597de05a99cdc4a46aa
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2018-05-23 00:13:09 +0500

    updated dependencies to fix vulnerability alerts

--- README.md
+++ README.md
@@ -1,26 +1,33 @@
 Node.JS HTTP Markup Server
 ==========================
 
-HTTP-server for markup (using [express](http://expressjs.com/) framework and [ejs](http://embeddedjs.com/) template engine)
+HTTP-server for markup (using [express](http://expressjs.com/) framework and
+[ejs](http://embeddedjs.com/) template engine)
 
 [![NPM](https://nodei.co/npm/http-markup-server.png)](https://nodei.co/npm/http-markup-server/)
+
+**WARNING!** This is a dead project.
+Any updates usually is just for supressing vulnerability alerts.
 
 Install
 =======
 
-```shell
-# npm install -g http-markup-server
+```bash
+npm install -g http-markup-server
 ```
 
 Usage
 =====
 
-Copy [config file (http-markup-server.config.json)](./http-markup-server.config.json) to your markup directory and start the server:
-```shell
-$ http-markup-server
+Copy
+[config file (http-markup-server.config.json)](http-markup-server.config.json)
+to your markup directory and start the server:
+
+```bash
+http-markup-server
 ```
 
 License
 =======
 
-[GNU/GPLv3 by Free Software Foundation](./LICENSE)
+[GPLv3](LICENSE)
--- bin/http-markup-server.js
+++ bin/http-markup-server.js
@@ -2,8 +2,8 @@
 /*!
  * HTTP server for markup
  *
- * Author: Viacheslav Lotsmanov
- * License: GNU/GPLv3 by Free Software Foundation
+ * Author: Viacheslav Lotsmanov <lotsmanov89@gmail.com>
+ * License: GPLv3
  * https://github.com/unclechu/node-http-markup-server/blob/master/LICENSE
  */
 
@@ -15,7 +15,7 @@ var spawn = require('child_process').spawn;
 var fs = require('fs');
 var url = require('url');
 
-var config = JSON.parse( fs.readFileSync('http-markup-server.config.json') );
+var config = JSON.parse(fs.readFileSync('http-markup-server.config.json'));
 
 var port = config.port;
 var host = config.host;
@@ -23,56 +23,35 @@ var openInBrowser = config.browse;
 
 var app = express();
 
+
+// parsing command-line arguments
 process.argv.slice(2).forEach(function (arg) {
-
     if (/^--port=/.test(arg)) {
-
         port = arg.substr('--port='.length);
-
     } else if (/^--host=/.test(arg)) {
-
         host = arg.substr('--host='.length);
-
     } else if (/^--browse=/.test(arg)) {
-
         openInBrowser = arg.substr('--browse='.length).toLowerCase();
 
-        if (openInBrowser == 'yes' || openInBrowser == 'y' || openInBrowser == '1') {
-
+        if (~['yes', 'y', '1'].indexOf(openInBrowser)) {
             openInBrowser = true;
-
-        } else if (openInBrowser == 'no' || openInBrowser == 'n' || openInBrowser == '0') {
-
+        } else if (~['no', 'n', '0'].indexOf(openInBrowser)) {
             openInBrowser = false;
-
         }
-
     } else {
-
         throw new Error('Unknown argument "'+ arg +'"');
-
     }
-
 });
 
-app.configure(function () {
 
-    config.static.forEach(function (val) {
+config.static.forEach(function (val) {
+    app.use('/' + val, express.static(val));
 
-        app.use('/' + val, express.static(val));
-
-        if (config.favicon && config.favicon === 'string') {
-
-            app.use(express.favicon( config.favicon ));
-
-        } else if (config.favicon) {
-
-            app.use(express.favicon('favicon.ico'));
-
-        }
-
-    });
-
+    if (config.favicon && config.favicon === 'string') {
+        app.use(express.favicon( config.favicon ));
+    } else if (config.favicon) {
+        app.use(express.favicon('favicon.ico'));
+    }
 });
 
 app.engine('html', ejs.renderFile);
@@ -81,7 +60,6 @@ app.set('views', './');
 app.set('view options', { layout: false });
 
 app.get('*', function (req, res) {
-
     var fullUrl = url.resolve('http://' + req.headers.host, req.url);
     var location = url.parse(fullUrl);
     var pathname = location.pathname;
@@ -92,47 +70,33 @@ app.get('*', function (req, res) {
     var curTplFilename;
 
     if (pathname.substr(-1) === '/') {
-
         curTplFilename = indexFiles.shift();
         tpl = path.join(pathname, curTplFilename);
         if (tpl.charAt(0) === '/') tpl = tpl.substr(1);
         dir = true;
-
     } else {
-
         if (path.extname(pathname) === '') {
-
-            res.send('403 Forbidden', 403);
+            res.status(403).send('403 Forbidden');
             if (config.security_log) console.warn('Hacking attempt!', req);
             return;
-
         } else {
-
             var found = false;
 
             config.allowable_extensions.forEach(function (ext) {
-
                 if (path.extname(pathname) === ext) {
-
                     found = true;
                     curTplFilename = path.basename(pathname);
                     tpl = path.join(pathname);
                     if (tpl.charAt(0) === '/') tpl = tpl.substr(1);
-
                 }
-
             });
 
             if (!found) {
-
-                res.send('403 Forbidden', 403);
+                res.status(403).send('403 Forbidden');
                 if (config.security_log) console.warn('Hacking attempt!', req);
                 return;
-
             }
-
         }
-
     }
 
     var relativeRoot = location.pathname;
@@ -149,14 +113,11 @@ app.get('*', function (req, res) {
     if (relativeRoot === '/') relativeRoot = './';
 
     function tryRender() {
-
         app.render(tpl, {
-
             RELATIVE_ROOT: relativeRoot, // relative path to root
             location: location,
 
             curNavPos: function curNavPos(withIndexPage) {
-
                 if (withIndexPage === undefined) withIndexPage = true;
                 withIndexPage = withIndexPage ? true : false;
 
@@ -164,79 +125,63 @@ app.get('*', function (req, res) {
                 var res = pathname;
 
                 if (res.substr(-filename.length) === filename) {
-
                     res = res.slice(0, -filename.length);
-
                 }
 
                 return res;
-
             },
-
         }, function (err, html) {
-
             if (err) {
                 if (err.toString().search(/failed to lookup view/i) !== -1) {
-
                     if (dir && indexFiles.length > 0) {
-
                         curTplFilename = indexFiles.shift();
                         tpl = path.join(pathname, curTplFilename);
                         if (tpl.charAt(0) === '/') tpl = tpl.substr(1);
                         process.nextTick( tryRender );
                         return;
-
                     } else {
+                        res.status(404).send('404 Not Found');
 
-                        res.send('404 Not Found', 404);
-                        if (config.security_log) console.warn('404 status.', req);
+                        if (config.security_log) {
+                            console.warn('404 status.', req);
+                        }
+
                         return;
-
                     }
-
                 } else {
+                    res
+                        .status(500)
+                        .send('500 Internal Server Error\n' + err.toString());
 
-                    res.send('500 Internal Server Error\n' + err.toString(), 500);
                     console.error(err);
                     return;
-
                 }
             }
 
             res.send(html);
-
         });
+    }
 
-    } tryRender();
-
+    tryRender();
 });
 
 app.listen(port, host, function () {
-
     var URL = 'http://';
 
     if (host) {
-
         console.log('Listening on %s:%d', host, port);
         URL += host;
-
     } else {
-
         console.log('Listening on *:%d', port);
         URL += '127.0.0.1';
-
     }
 
     if (port !== 80) URL += ':' + port;
-    if (openInBrowser) {
 
+    if (openInBrowser) {
         spawn('xdg-open', [URL], { stdio: 'ignore' })
             .on('error', function (err) {
                 console.log('Open link in browser error:', err.toString());
             });
-
     }
-
 });
-
-// vim: set ts=4 sw=4 et :
